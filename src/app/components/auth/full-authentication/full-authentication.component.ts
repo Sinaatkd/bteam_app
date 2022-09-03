cc881f6c04194fa8a4f4e363e83240603b9f07f0
Sina <general.sina.amini.20@gmail.com>
2022-09-03 13:58:40 +0430

    It is easier to use the date of birth input

--- src/app/components/auth/full-authentication/full-authentication.component.ts
+++ src/app/components/auth/full-authentication/full-authentication.component.ts
@@ -12,11 +12,11 @@ import { BASE_URL } from 'src/app/utilities/variables';
 })
 export class FullAuthenticationComponent implements AfterViewInit {
   @ViewChild(IonSlides) slides: IonSlides;
+
+  inputType = '';
   user: UserModel;
   userInformationForm: FormGroup;
   isAcceptRules = false;
-  previusLnegthDateOfBirthInput = 0;
-  previusValueDateOfBirthInput = 0;
   selectedIDCardPicture = null;
   selectedFacePicture = null;
   BASE_URL = BASE_URL;
@@ -24,48 +24,61 @@ export class FullAuthenticationComponent implements AfterViewInit {
 
   slideOpts = {
     initialSlide: 0,
-    speed: 400
+    speed: 400,
   };
 
   constructor(
     public userService: UserService,
-    public loadingCtrl: LoadingController,
-  ) {
-  }
+    public loadingCtrl: LoadingController
+  ) {}
 
   ngAfterViewInit() {
-    this.loadingCtrl.create({
-      message: 'لطفا صبر کنید',
-      mode: 'ios',
-    }).then(loadingEl => {
-      loadingEl.present()
-      this.userService.getUser().subscribe(user => {
-        this.user = user;
-        if (user.user.id_card) {
-          this.selectedIDCardPicture = BASE_URL + user.user.id_card;
-        }
-        this.userInformationForm = new FormGroup({
-          full_name: new FormControl(this.user.user.full_name, { validators: [Validators.required] }),
-          phone_number: new FormControl(this.user.user.phone_number, { validators: [Validators.required] }),
-          national_code: new FormControl(this.user.user.national_code, { validators: [Validators.required] }),
-          date_of_birth: new FormControl(this.user.user.date_of_birth, { validators: [Validators.required] }),
-          father_name: new FormControl(this.user.user.father_name, { validators: [Validators.required] }),
-          place_of_issue: new FormControl(this.user.user.place_of_issue, { validators: [Validators.required] }),
-        });
-        setTimeout(() => {
-          if (user.user.id_card && user.user.face) {
-            this.slides.length().then((value) => {
-              this.slides.lockSwipes(false).then();
-              this.slides.slideTo(value + 1).then();
-              this.currentSlide = 6;
-              this.slides.lockSwipes(true).then();
-            })
+    this.loadingCtrl
+      .create({
+        message: 'لطفا صبر کنید',
+        mode: 'ios',
+      })
+      .then((loadingEl) => {
+        loadingEl.present();
+        this.userService.getUser().subscribe((user) => {
+          this.user = user;
+          if (user.user.id_card) {
+            this.selectedIDCardPicture = BASE_URL + user.user.id_card;
           }
-          this.slides.lockSwipes(true).then();
-          loadingEl.dismiss();
-        }, 1000);
+          this.userInformationForm = new FormGroup({
+            full_name: new FormControl(this.user.user.full_name, {
+              validators: [Validators.required],
+            }),
+            phone_number: new FormControl(this.user.user.phone_number, {
+              validators: [Validators.required],
+            }),
+            national_code: new FormControl(this.user.user.national_code, {
+              validators: [Validators.required],
+            }),
+            date_of_birth: new FormControl(this.user.user.date_of_birth, {
+              validators: [Validators.required],
+            }),
+            father_name: new FormControl(this.user.user.father_name, {
+              validators: [Validators.required],
+            }),
+            place_of_issue: new FormControl(this.user.user.place_of_issue, {
+              validators: [Validators.required],
+            }),
+          });
+          setTimeout(() => {
+            if (user.user.id_card && user.user.face) {
+              this.slides.length().then((value) => {
+                this.slides.lockSwipes(false).then();
+                this.slides.slideTo(value + 1).then();
+                this.currentSlide = 6;
+                this.slides.lockSwipes(true).then();
+              });
+            }
+            this.slides.lockSwipes(true).then();
+            loadingEl.dismiss();
+          }, 1000);
+        });
       });
-    })
   }
 
   moveToNextSlide() {
@@ -75,31 +88,35 @@ export class FullAuthenticationComponent implements AfterViewInit {
     this.slides.lockSwipes(true).then();
   }
 
-
   acceptRules(event) {
     this.isAcceptRules = event.detail.checked;
   }
 
   saveData() {
-    this.loadingCtrl.create({
-      message: 'لطفا صبر کنید',
-      mode: 'ios',
-    }).then(loadingEl => {
-      loadingEl.present()
-      const inputs = this.userInformationForm.controls;
-      this.user.user.date_of_birth = inputs.date_of_birth.value;
-      this.user.user.father_name = inputs.father_name.value;
-      this.user.user.place_of_issue = inputs.place_of_issue.value;
-      this.user.user.id_card = this.selectedIDCardPicture
-      this.user.user.face = this.selectedFacePicture
-      this.userService.editUserInfo(this.user).subscribe(res => {
-        loadingEl.dismiss();
-        this.moveToNextSlide();
-      }, err => {
-        loadingEl.dismiss();
-        console.log(err);
+    this.loadingCtrl
+      .create({
+        message: 'لطفا صبر کنید',
+        mode: 'ios',
       })
-    })
+      .then((loadingEl) => {
+        loadingEl.present();
+        const inputs = this.userInformationForm.controls;
+        this.user.user.date_of_birth = inputs.date_of_birth.value;
+        this.user.user.father_name = inputs.father_name.value;
+        this.user.user.place_of_issue = inputs.place_of_issue.value;
+        this.user.user.id_card = this.selectedIDCardPicture;
+        this.user.user.face = this.selectedFacePicture;
+        this.userService.editUserInfo(this.user).subscribe(
+          (res) => {
+            loadingEl.dismiss();
+            this.moveToNextSlide();
+          },
+          (err) => {
+            loadingEl.dismiss();
+            console.log(err);
+          }
+        );
+      });
   }
 
   IDCardPictureChanged(event) {
@@ -114,8 +131,7 @@ export class FullAuthenticationComponent implements AfterViewInit {
     reader.onload = function () {
       me.selectedIDCardPicture = reader.result;
     };
-    reader.onerror = function (error) {
-    };
+    reader.onerror = function (error) {};
   }
   getBase64Face(file) {
     let me = this;
@@ -123,10 +139,9 @@ export class FullAuthenticationComponent implements AfterViewInit {
     reader.readAsDataURL(file);
     reader.onload = function () {
       me.selectedFacePicture = reader.result;
-      me.saveData()
+      me.saveData();
     };
-    reader.onerror = function (error) {
-    };
+    reader.onerror = function (error) {};
   }
 
   facePictureChanged(event) {
@@ -134,4 +149,37 @@ export class FullAuthenticationComponent implements AfterViewInit {
     this.getBase64Face(file);
   }
 
+  ionInputCalled(event) {
+    if (event.type === 'ionInput') {
+      this.inputType = event.detail.inputType;
+    } else {
+      const inputValueLength = event.detail.value.length;
+      // deleteContentBackward
+      //  insertText
+      console.log(inputValueLength);
+
+      if (
+        (inputValueLength === 4 || inputValueLength === 7) &&
+        this.inputType === 'insertText'
+      ) {
+        this.userInformationForm.controls.date_of_birth.setValue(
+          event.detail.value + '-'
+        );
+      } else if (
+        (inputValueLength === 5 || inputValueLength === 8) &&
+        this.inputType === 'deleteContentBackward'
+      ) {
+        const newValue =
+          this.userInformationForm.controls.date_of_birth.value.slice(0, -2);
+        this.userInformationForm.controls.date_of_birth.setValue(newValue);
+      } else if (
+        (inputValueLength === 4 || inputValueLength === 7) &&
+        this.inputType === 'deleteContentBackward'
+      ) {
+        const newValue =
+          this.userInformationForm.controls.date_of_birth.value.slice(0, -1);
+        this.userInformationForm.controls.date_of_birth.setValue(newValue);
+      }
+    }
+  }
 }
